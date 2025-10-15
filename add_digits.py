1a85191846f320dd8761010fe463c52a10b3efb3
Julian Jung <tick.73spatial@icloud.com>
2025-10-15 11:44:17 +0200

    Create add_digits.py

---
+++ add_digits.py
@@ -0,0 +1,8 @@
+"""
+Write a function that takes accepts one parameter.
+The given Parameter will be a string of a multi-digit number.
+The function should return the product of all digits.
+
+1234 -> 24
+1111 -> 1
+"""
