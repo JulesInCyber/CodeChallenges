c93c87ff2139778f478b2c7020e8bdfeda2d38c0
Julian Jung <tick.73spatial@icloud.com>
2025-10-15 11:44:13 +0200

    Create rad_to_deg.py

---
+++ rad_to_deg.py
@@ -0,0 +1,5 @@
+"""
+Write a function in Python that accepts one numeric parameter.
+This parameter will be the measure of an angle in radians.
+The function should convert the radians into degree and return that value.
+"""
