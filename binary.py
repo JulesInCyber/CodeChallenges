e77485e940a5e18cee60fd31fba479904a1f04a0
Julian Jung <tick.73spatial@icloud.com>
2025-10-16 13:00:13 +0200

    Create binary.py

---
+++ binary.py
@@ -0,0 +1,8 @@
+"""
+Write a Function, that accepts two parameters.
+The first parameter will be indicating if the function should transform
+    - TO binary or
+    - FROM binary
+The second parameter will be a number that is either binary or decimal
+"""
+
