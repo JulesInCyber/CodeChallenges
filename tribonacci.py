215eefcfa15d73b564c5ededda3cf1deb8e867bd
Julian Jung <tick.73spatial@icloud.com>
2025-10-16 13:00:09 +0200

    Create tribonacci.py

---
+++ tribonacci.py
@@ -0,0 +1,8 @@
+"""
+The "Tribonacci Sequence" challenge is a twist on the famous
+Fibonacci Sequence, where each number is the sum of the preceding three numbers
+
+for example: 0,1,1,2,4,7 ...
+
+Write a function that returns the nth number in the Tribonacci Sequence
+""
