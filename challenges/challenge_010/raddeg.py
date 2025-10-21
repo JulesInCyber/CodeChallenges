aebfee15cd837885cd171b99a50d226f8dab53b4
Julian Jung <tick.73spatial@icloud.com>
2025-10-21 14:44:19 +0200

    deg to rad

--- challenges/challenge_010/raddeg.py
+++ challenges/challenge_010/raddeg.py
@@ -6,4 +6,9 @@ def rad_to_deg(radians):
     degree = round(degree, 3)
     return degree
 
+def deg_to_rad(degree):
+    radians = degree * math.pi / 180
+    return radians
+
 print(rad_to_deg(1))
+print(deg_to_rad(57.3))
