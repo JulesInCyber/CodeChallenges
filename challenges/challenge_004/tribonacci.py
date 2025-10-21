65685a1ebe0ac8fc1f5addf3f5625fd3348e0b30
Julian Jung <tick.73spatial@icloud.com>
2025-10-21 09:10:29 +0200

    tribonacci solution

---
+++ challenges/challenge_004/tribonacci.py
@@ -0,0 +1,21 @@
+def tribonacci(pos):
+
+    # Special Cases
+    if pos == 0:
+        return 0
+    elif pos == 1 or pos == 2:
+        return 1
+    
+    # Initializing the first three numbers
+    t0 = 0
+    t1 = 1
+    t2 = 1
+
+    for i in range(3, pos+1):
+        t_pos = t0 + t1 + t2
+        t0 = t1
+        t1 = t2
+        t2 = t_pos
+    return t_pos
+    
+print(tribonacci(0))
