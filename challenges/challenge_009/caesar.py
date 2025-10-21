bfdcd1bff1daf4809b788eb5b01f66f13778cc68
Julian Jung <tick.73spatial@icloud.com>
2025-10-21 11:31:46 +0200

    caesar encryption

---
+++ challenges/challenge_009/caesar.py
@@ -0,0 +1,17 @@
+def caesar_eccryption(message, shift):
+    enc_message = ''
+
+    for letter in message:
+        if letter.isalpha():
+            if letter.isupper():
+                start = ord("A")
+            else:
+                start = ord("a")
+            enc_letter = chr((ord(letter) - start + shift) % 26 + start)
+            enc_message += enc_letter
+        else:
+            enc_message += letter
+    return enc_message
+
+print(caesar_eccryption("Hello World!", 13))
+# print(caesar_decryption("Uryyb Jbeyq!", 13))
---
+++ challenges/challenge_009/task.md
@@ -0,0 +1,15 @@
+# Challenge 009: Caesar Encryption
+
+**Difficulty**: Medium
+
+**Tags**: #cryptographie
+
+## Task
+
+Write a function that accepts two parameters:
+    - A string to be encoded
+    - an integer representing the number of positions each letter should be shifted
+
+_Bonus_: Write another function that decrypts the message.
+
+ 
